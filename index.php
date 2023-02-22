28dadad4e2de38314f8f61c2b48149f40f788064
Zachary Stocks <zachary.stocks@outlook.com>
2023-02-22 12:52:26 -0700

    Adds basic content to projects section.

--- index.php
+++ index.php
@@ -30,35 +30,26 @@
             </div>
         </div>
 
-        <!-- <div id="nav">
-            <p id="name">Zachary Stocks</p>
-            <ul id="personal-links">
-                <li><a id="resume-btn" href="#">View Resume</a></li>
-            </ul>
-        </div>
-
-        <div class="container" id="intro">
-            <img src="img/zachary-stocks-portrait.jpg" width="400" height="600" alt="Profile image of Zachary Stocks.">
-            <div>
-                <h1>Hi, I'm Zach</h1>
-                <p>I'm a web developer. I've spent most of my days developing and maintaining websites for the <a href="https://www.usu.edu/utahstars/about/index" target="_blank">USU STARS! GEAR UP program</a> at Utah State University. I've been involved with each phase of the development lifecycle across various projects. I'm familiar with traditional as well as object-oriented approaches to development, and have worked with both the front- and back-ends of applications.</p>
-                <p>My experience includes work with front-end tools like HTML, CSS, and JavaScript as well as a proficiency with Bootstrap, Foundation, and jQuery. I aim to harness the potential of JavaScript by currently improving my understanding of its major front-end frameworks - namely Vue, React, and Angular.</p>
-                <p>My experience with back-end tools includes an understanding of relational databases such as MySQL, some experience with Node.js, and an understanding of PHP to liaison between the client and server sides of an application.</p>
-                <p>Feel free to check out my full resume if you're interested in learning more about my experience. I'd also love to connect:<br>→ <a href="https://github.com/zstocks" target="_blank">GitHub</a><br>→ <a href="https://www.linkedin.com/in/zachary-stocks/" target="_blank">LinkedIn</a></p>
-                <hr>
-                <p>I believe success as a developer demands a willingness to continually learn and improve. I rely daily upon tools like <a href="https://developer.mozilla.org/" target="_blank">MDN</a> and <a href="https://stackoverflow.com/" target="_blank">Stack Overflow</a> to find solutions to problems I encounter. I'm always expanding my learning with tools like <a href="https://learnjavascript.online/" target="_blank">Learn JavaScript</a>, <a href="https://teamtreehouse.com/" target="_blank">Treehouse</a>, <a href="https://leetcode.com/" target="_blank">LeetCode</a>, and others.</p> 
-                <p>I aim to use this website as a place to showcase the projects that I've built as I continue to improve my skillset and challenge myself in new ways.</p> 
-                <p>Check out some of my projects below ↓</p>
-            </div>
-        </div> -->
-
         <div class="container" id="projects">
             <ul>
                 <h2>My Projects</h2>
                 <li class="project-tab is-active">
                     <a href="#" class="project-title">USU STARS! GEAR UP Website</a>
                     <div class="project-content">
-                        All project info goes here.
+                        <div>
+                            <div class="project-details">
+                                <img src="https://via.placeholder.com/250" alt="project screenshot image."><br>
+                                <a href="#">Button Here</a>
+                                <ul>
+                                    <li>HTML</li>
+                                    <li>CSS</li>
+                                    <li>OmniCMS (Content Management System)</li>
+                                </ul>
+                            </div>
+                            <div class="project-summary">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae incidunt iusto unde tempore voluptate officiis rem consequuntur deserunt assumenda perferendis, sunt accusamus, asperiores placeat accusantium porro molestias possimus facilis obcaecati.</p>
+                            </div>
+                        </div>
                     </div>
                 </li>
                 <li class="project-tab">
